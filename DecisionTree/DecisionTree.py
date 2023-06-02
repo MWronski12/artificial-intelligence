2089d0149825fe99b81c8a61392e289eab0b439b
MWronski12 <wronski.mikolaj@gmail.com>
2023-06-02 11:54:23 +0200

    Refactoring

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
@@ -22,8 +21,9 @@ class DecisionTree:
         self.max_depth = max_depth
 
     def _entropy(self, y):
-        proportions = np.bincount(y) / len(y)
-        entropy = -sum([p * math.log(p) for p in proportions if p > 0])
+        _, counts = np.unique(y, return_counts=True)
+        proportions = counts / len(y)
+        entropy = -np.sum(proportions * np.log(proportions))
         return entropy
 
     def _information_gain(self, y, subset1, subset2):
@@ -39,21 +39,20 @@ class DecisionTree:
         return ig
 
     def _split(self, X, feature, threshold):
-        left_idx = np.argwhere(np.array(X)[:, feature] < threshold).flatten()
-        right_idx = np.argwhere(np.array(X)[:, feature] >= threshold).flatten()
+        left_idx = X[:, feature] < threshold
+        right_idx = X[:, feature] >= threshold
         return left_idx, right_idx
 
     def _get_best_split(self, X, y):
         best_split = {"score": -1, "feature": -1, "threshold": -1}
 
-        features = [i for i in range(np.shape(X)[1])]
-        for feature in features:
-            thresholds = np.unique(np.array(X)[:, feature])
+        for feature in range(X.shape[1]):
+            thresholds = np.unique(X[:, feature])
 
             for threshold in thresholds:
                 left_idx, right_idx = self._split(X, feature, threshold)
-                subset1 = [y[i] for i in left_idx]
-                subset2 = [y[i] for i in right_idx]
+                subset1 = y[left_idx]
+                subset2 = y[right_idx]
                 ig = self._information_gain(y, subset1, subset2)
                 if (ig > best_split["score"]):
                     best_split["score"] = ig
@@ -74,47 +73,44 @@ class DecisionTree:
         left_idx, right_idx = self._split(X, feature, threshold)
 
         node.left = Node()
-        X_left = [X[i] for i in left_idx]
-        y_left = [y[i] for i in left_idx]
+        X_left = X[left_idx]
+        y_left = y[left_idx]
         self._build_tree(node.left, X_left, y_left, depth - 1)
 
         node.right = Node()
-        X_right = [X[i] for i in right_idx]
-        y_right = [y[i] for i in right_idx]
+        X_right = X[right_idx]
+        y_right = y[right_idx]
         self._build_tree(node.right, X_right, y_right, depth - 1)
 
+    def _traverse(self, x, node):
+        node = self.root
+        while not node.is_leaf():
+            if x[node.feature] < node.threshold:
+                node = node.left
+            else:
+                node = node.right
+        return node.value
+    
     def fit(self, X, y):
         self.root = Node()
         self._build_tree(self.root, X, y, self.max_depth)
 
-    def _traverse(self, x, node):
-        if (node.is_leaf()):
-            return node.value
-
-        if (x[node.feature] < node.threshold):
-            return self._traverse(x, node.left)
-        else:
-            return self._traverse(x, node.right)
-
     def predict(self, X):
-        results = []
-        for x in X:
-            results.append(self._traverse(x, self.root))
-        return results
+        return [self._traverse(x, self.root) for x in X]
 
 
 def main():
     data = datasets.load_breast_cancer()
     X, y = data.data, data.target
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=1
+        X, y, test_size=0.2, random_state=42
     )
 
     clf = DecisionTree(max_depth=10)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
-    acc = np.sum(y_test == y_pred) / len(y_test)
 
+    acc = np.sum(y_test == y_pred) / len(y_test)
     print("Accuracy:", acc)
 
 
